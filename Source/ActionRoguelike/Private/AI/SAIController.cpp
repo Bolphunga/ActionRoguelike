c61ba07e0aa6860cfab29bd434cadc0831567423
aymendj <togoro>
2024-05-18 03:50:42 +0200

    Added PawnSensing to AI character
    Added PawnSensing to AI character
    Improved AI character animation

--- Source/ActionRoguelike/Private/AI/SAIController.cpp
+++ Source/ActionRoguelike/Private/AI/SAIController.cpp
@@ -12,10 +12,10 @@ void ASAIController::BeginPlay()
 
 	APawn* MyPawn = UGameplayStatics::GetPlayerPawn(this, 0);
 
-	if (MyPawn)
-	{
-		GetBlackboardComponent()->SetValueAsVector("MoveToLocation", MyPawn->GetActorLocation());
+	//if (MyPawn)
+	//{
+	//	GetBlackboardComponent()->SetValueAsVector("MoveToLocation", MyPawn->GetActorLocation());
 
-		GetBlackboardComponent()->SetValueAsObject("TargetActor", MyPawn);
-	}
+	//	GetBlackboardComponent()->SetValueAsObject("TargetActor", MyPawn);
+	//}
 }
